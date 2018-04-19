8dc74cae99d17a5d91540fd8ee204c3a5768ab10
Sonampandey11 <38416714+Sonampandey11@users.noreply.github.com>
2018-04-19 18:18:49 +0530

    Add files via upload

---
+++ PMD/src/main/java/mavenproject/javaprogram/GenericArray.java
@@ -0,0 +1,32 @@
+package com.cg.pmd;
+
+//*********************************************************************** 
+//	Program name : 			ArrayElement
+//	Program Description :	using single method print arraythat can print all 
+//                            the elements of both string and integer array 
+//                        
+//***********************************************************************
+
+public class GenericArray {
+	public static void main(String[] args) {
+		
+    //creating an array of integer and string
+	final Integer[] number= {1,2,3,4};
+    final String[] name= {"Hello", "World"};
+   //pass an integer and string array
+     displayArray(number);
+     displayArray(name);
+	}
+	
+   //creating an generic method
+	static <E> void displayArray(E[] input)
+	{
+		for(final E element: input)
+		{
+			//display element integer and charcter
+			System.out.printf("%s",element);
+		}
+		System.out.println();
+	}
+
+}
