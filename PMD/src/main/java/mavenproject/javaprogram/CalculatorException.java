50d693cd19d0a9c7ce6f30475d7b18fb8688606e
Sonampandey11 <38416714+Sonampandey11@users.noreply.github.com>
2018-04-19 20:13:06 +0530

    Add files via upload

---
+++ PMD/src/main/java/mavenproject/javaprogram/CalculatorException.java
@@ -0,0 +1,14 @@
+package com.cg.pmd;
+
+//*********************************************************************** 
+//	Program name : 			Calculator
+//	Program Description :	creating an user defined exception  for calculator
+//                      
+//***********************************************************************
+public class CalculatorException extends Exception {
+
+	public CalculatorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
