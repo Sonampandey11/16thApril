fbadd0580ba135f96465ba71ddf25848b5986b09
Sonampandey11 <38416714+Sonampandey11@users.noreply.github.com>
2018-04-20 13:27:36 +0530

    Add files via upload

---
+++ PMD Junit Test case/main/java/mavenproject/javaprogram/CalculatorException.java
@@ -0,0 +1,14 @@
+package mavenproject.javaprogram;
+
+//*********************************************************************** 
+//	Program name : 			Calculator
+//	Program Description :	creating an user defined exception  for calculator
+//                      
+//***********************************************************************
+public class CalculatorException extends Exception {
+
+	public CalculatorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
